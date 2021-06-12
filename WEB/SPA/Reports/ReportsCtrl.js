a831f353d55199af6d2ed1b7b6c1a7a30bb17e90
jubair hridoy <jubair_29@outlook.com>
2021-06-12 15:02:23 +0600

    12/06/2021

--- WEB/SPA/Reports/ReportsCtrl.js
+++ WEB/SPA/Reports/ReportsCtrl.js
@@ -35,7 +35,7 @@
                 $scope.imageLocationList = data;
 
                 angular.forEach(data, function (obj) {
-                    window.open('http://202.126.122.85:72' + obj.ImageLocation);
+                    window.open('http://api.infinigentconsulting.com' + obj.ImageLocation);
                 });
                 //window.open('http://202.126.122.85:72/image/201_ICL_100017_1.png');
                 //  window.open('http://api.infinigentconsulting.com' + obj.ImageLocation);
